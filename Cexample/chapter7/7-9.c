259eb759010ac020a16081be984ed35c4c588622
Taejun-Git <gotaejun@gmail.com>
2025-01-02 13:32:36 +0000

    commit

---
+++ Cexample/chapter7/7-9.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int check;
+
+    printf("2이상의 정수를 입력하시오:");
+    
+    while ((check = scanf("%d", &num)) == 1)
+    {
+        int i, j;
+        for (i = 2; i <= num; i++)
+        {
+            int count = 0;
+            for (j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                    ++count;
+            }
+            
+            if (count == 2)
+                printf("%d ", i);
+        }
+        printf("\n2이상의 정수를 입력하시오.");
+    }
+    return 0;
+}
