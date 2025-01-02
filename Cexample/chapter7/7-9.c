a79bec7bdbb655fb1acdced426dc17c46e9ef282
Taejun-Git <gotaejun@gmail.com>
2025-01-02 13:49:38 +0000

    add 7-9

--- Cexample/chapter7/7-9.c
+++ Cexample/chapter7/7-9.c
@@ -13,16 +13,16 @@ int main()
         for (i = 2; i <= num; i++)
         {
             int count = 0;
-            for (j = 1; j <= i; j++)
+            for (j = 1; j <= i; j++)  // for (int div = 2, numIsPrime = true; (div * div) <= num; div++
             {
-                if (i % j == 0)
-                    ++count;
-            }
-            
+                if (i % j == 0)       // if (num % div == 0)
+                    ++count;          // numIsPrime = false;
+            }                         // if (numIsPrime)
+                                      // printf("%d is prime\n", num);
             if (count == 2)
                 printf("%d ", i);
         }
-        printf("\n2이상의 정수를 입력하시오.");
+        printf("\n2이상의 정수를 입력하시오.(q 입력시 종료): ");
     }
     return 0;
 }
