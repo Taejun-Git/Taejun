698cc48a32812db132ed1503317f8d76e7bc142b
Taejun-Git <gotaejun@gmail.com>
2025-01-19 13:16:54 +0000

    add 8-03

--- Cexample/chapter8/8-03.c
+++ Cexample/chapter8/8-03.c
@@ -11,9 +11,9 @@ int main(void)
     {
         putchar(ch);
 
-        if (islower(ch) != 0)
+        if (islower(ch)) // islower(ch) 만 해도 되네 -> 참이면 lower오를꺼고 거짓이면 안오르는 건 마찬가지니까((islower(ch) != 0) == islower(ch)))
             ++lower;
-        else if (isupper(ch) != 0)
+        else if (isupper(ch))
             ++upper;
         else
             continue;
